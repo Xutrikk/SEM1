ed005521e3d5418d905bf0139ce6f702b62c28fb
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-02-29 01:41:56 +0300

    Add files via upload

---
+++ OAP/lab11/lab11.1oap.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>//Подключение библиотеки для работы с вводом/выводом
+int main()
+{
+    using namespace std;// Использование пространства имен std
+    setlocale(LC_CTYPE, "Russian");//Функция, с помощью которой корректно отображается русский язык
+    int A, n, p; char tmp[33];
+    cout << "Введите число А ";//Вывод на экран
+    cin >> A;
+    cout << endl;
+    _itoa_s(A, tmp, 2);
+    cout << "Число в двоичном виде = " << tmp << endl;//Вывод на экран
+    cout << " " << endl;
+    cout << "Введите от какого бита начинать замену ";//Вывод на экран
+    cin >> p;
+    cout << endl;
+    cout << "Скольно заменяем битов? ";//Вывод на экран
+    cin >> n;
+    cout << endl;
+    unsigned int mask = (1 << p) - 1;
+    for (int i = 0; i < n; i++) {
+        A ^= mask;
+        mask <<= 1;
+    }
+    _itoa_s(A, tmp, 2);
+    cout << "Итоговое число = " << tmp << endl;//Вывод на экран
+}
+
