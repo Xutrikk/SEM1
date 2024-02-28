53fc244e00fc2054840c3756b17c459355a0846e
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-02-29 01:42:32 +0300

    Add files via upload

---
+++ OAP/lab12/lab12.1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>//Подключение библиотеки для работы с вводом/выводом
+void main() {
+	using namespace std;// Использование пространства имен std
+	setlocale(LC_CTYPE, "Russian");//Функция, с помощью которой корректно отображается русский язык
+	const int n = 10;
+	int F[n], D[n], sum, a = 0, i, size;
+
+	cout << "Массив F: " << endl;//Вывод на экран
+	for (i = 0; i < n; i++)
+	{
+		*(F + i) = rand() % 100;
+		cout << " " << F[i];
+	}
+	cout << endl;
+	cout << "Массив D: " << endl;//Вывод на экран
+	for (i = 0; i < n; i++)
+	{
+		*(D + i) = rand() % 100;
+		cout << " " << D[i];
+	}
+
+	int max = -99;
+	for (i = 0; i < n; i++)
+	{
+		if (F[i] > max)
+		{
+			max = F[i];
+		}
+	}
+	cout << endl;
+	cout << "Max = " << max << std::endl;//Вывод на экран
+	for (i = 0; i < n; i++)
+	{
+		if (D[i] == max)
+		{
+			a = 1;
+		}
+	}
+	if (a == 1)
+	{
+		cout << "есть";//Вывод на экран
+	}
+	if (a == 0)
+	{
+		cout << "нет";//Вывод на экран
+	}
+}
---
+++ OAP/lab12/lab12oap.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>//Подключение библиотеки для работы с вводом/выводом
+#include <ctime>
+void main()
+{
+	using namespace std;// Использование пространства имен std
+	setlocale(LC_CTYPE, "Russian");//Функция, с помощью которой корректно отображается русский язык
+	srand(time(NULL));
+	int n, rmx = 100, rmn = -100;
+	const int sz = 1000;
+	int A[sz] = {};
+	cout << "Размер массива: ";//Вывод на экран
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		*(A + (i)) = (int)(((double)rand() /
+			(double)RAND_MAX) * (rmx - rmn) + rmn);
+		cout << "A[" << i << "]" << "\t" << A[i] << endl;//Вывод на экран
+	}
+	cout << " " << endl;
+	cout << "Массив после изменения: " << endl;//Вывод на экран
+	for (int i = 0; i < n; i++)
+	{
+		if (*(A + (i)) > 0)
+		{
+			cout << *(A + (i)) << endl;//Вывод на экран
+		}
+	}
+	for (int j = 0; j < n; j++)
+	{
+		if (*(A + (j)) < 0)
+		{
+			cout << *(A + (j)) << endl;//Вывод на экран
+		}
+	}
+}
