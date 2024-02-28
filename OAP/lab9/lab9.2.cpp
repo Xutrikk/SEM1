3270f865669260016b15407231c471f97052f450
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-02-29 01:40:37 +0300

    Add files via upload

---
+++ OAP/lab9/lab9.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>//Подключение библиотеки для работы с вводом/выводом
+void main()
+{
+	using namespace std;// Использование пространства имен std
+	setlocale(LC_CTYPE, "Russian");//Функция, с помощью которой корректно отображается русский язык
+	double a = -2, b = -1, e = 0.0001, x;//Ввод данных
+	while (abs(a - b) > (2 * e))
+	{
+		x = (a + b) / 2;
+		if ((sin(a) + 2 + a) * (sin(x) + 2 + x) <= 0) b = x;
+		else a = x;
+	}
+	cout << "x = " << x << endl;//Вывод на экран 
+}
---
+++ OAP/lab9/lab9oap.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>//Подключение библиотеки для работы с вводом/выводом
+void main()
+{
+	using namespace std;// Использование пространства имен std
+	setlocale(LC_CTYPE, "Russian");//Функция, с помощью которой корректно отображается русский язык
+	double h, x, s = 0, a = 2, b = 7, n = 200;//Ввод данных
+	h = (b - a) / n;
+	x = a;
+	for (x; x < (b - h); x += h)
+	{
+		s = s + h * (pow(sin(x), 2) + 1 + pow(sin(x + h), 2) + 1);
+		x = x + h;
+	}
+	cout << "s = " << s << endl;//Вывод на экран 
+}
